50341a850ad27f07645ed8baf91127a6ece2e777
Gabydc <diazcortesgb@gmail.com>
2018-02-15 19:08:15 +0100

    Version 2/15

--- pushrepo.sh
+++ pushrepo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add *
-git commit -m "updated programs"
-git remote add origin https://github.com/Gabydc/Programs_January_18
+git commit -m "Version 2/15"
+git remote add origin https://github.com/Gabydc/2018_February.git
 git push -u origin master
